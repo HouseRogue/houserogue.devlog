37064682e3662cae9a4683f794e01f2df542bbd5
HouseRogue <houserogue.dev@gmail.com>
2025-08-07 03:03:13 +0200

    .

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,22 @@
-const baseUrl = "/houserogue.devlog";
+document.addEventListener("DOMContentLoaded", () => {
+  const baseUrl = "/houserogue.devlog";
 
-const coverImage = document.getElementById("cover-image");
-const firstPost = document.querySelector(".post-button[data-cover]");
-const defaultSrc = firstPost
-  ? baseUrl + "/" + firstPost.dataset.cover
-  : coverImage.src;
+  const coverImage = document.getElementById("cover-image");
+  const firstPost = document.querySelector(".post-button[data-cover]");
+  const defaultSrc = firstPost
+    ? baseUrl + "/" + firstPost.dataset.cover
+    : coverImage.src;
 
-coverImage.src = defaultSrc;
-let lastHoveredSrc = defaultSrc;
+  coverImage.src = defaultSrc;
+  let lastHoveredSrc = defaultSrc;
 
-document.querySelectorAll(".post-button").forEach((button) => {
-  button.addEventListener("mouseenter", () => {
-    const newSrc = button.dataset.cover;
-    if (newSrc) {
-      coverImage.src = baseUrl + "/" + newSrc;
-      lastHoveredSrc = baseUrl + "/" + newSrc;
-    }
+  document.querySelectorAll(".post-button").forEach((button) => {
+    button.addEventListener("mouseenter", () => {
+      const newSrc = button.dataset.cover;
+      if (newSrc) {
+        coverImage.src = baseUrl + "/" + newSrc;
+        lastHoveredSrc = baseUrl + "/" + newSrc;
+      }
+    });
   });
 });
