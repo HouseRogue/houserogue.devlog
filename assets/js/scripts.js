cd182e3acbfa6c337624eab5364b698b0532acd9
HouseRogue <houserogue.dev@gmail.com>
2025-08-07 02:54:01 +0200

    lookin good

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,16 @@
+const coverImage = document.getElementById("cover-image");
+const firstPost = document.querySelector(".post-button[data-cover]");
+const defaultSrc = firstPost ? firstPost.dataset.cover : coverImage.src;
 
+coverImage.src = defaultSrc;
+let lastHoveredSrc = defaultSrc;
+
+document.querySelectorAll(".post-button").forEach((button) => {
+  button.addEventListener("mouseenter", () => {
+    const newSrc = button.dataset.cover;
+    if (newSrc) {
+      coverImage.src = newSrc;
+      lastHoveredSrc = newSrc;
+    }
+  });
+});
